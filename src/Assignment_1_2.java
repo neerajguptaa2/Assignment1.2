fb54cc7a3c77d135e41d7cb3c49c22d7cd79a9b2
neerajguptaa2 <neerajguptaa@ymail.com>
2016-11-24 03:12:38 +0530

    Assignment

---
+++ src/Assignment_1_2.java
@@ -0,0 +1,24 @@
+
+public class Assignment_1_2 {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 10;
+		
+		System.out.println("value of a is = " + a);
+		
+		b= a-- - --a;
+		System.out.println("value of b is = " + b);
+		
+		int c=a--;
+		System.out.println("value of c is = " + c);
+		
+		int d=a>>2;
+		System.out.println("value of d is = " + d);
+		
+		int e=a&b;
+		System.out.println("value of e is = " + e);
+	}
+
+}
